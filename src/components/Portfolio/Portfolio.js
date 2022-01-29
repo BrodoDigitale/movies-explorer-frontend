51a296509196b64f9055e79a44b5f1e9f6fc00c1
Julia Sensi <sensi.julia@gmail.com>
2022-01-29 14:59:56 +0100

    feat: add jsx and style for Portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,44 @@
-import "./Portfolio.css"
-
-
+import "./Portfolio.css";
+import { Link } from "react-router-dom";
+import { SectionHeader } from "../SectionHeader/SectionHeader";
+import arrow from "../../images/portfolio-arrow.svg";
 
 export function Portfolio() {
-    return(
-        <section className="Portfolio">
-        </section>
-    )
+  return (
+    <section className="portfolio">
+      <SectionHeader title="Портфолио" modificator="sectionHeader_portfolio"/>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <Link className="portfolio__list-link" to="https://brododigitale.github.io/how-to-learn/">
+            <p className="portfolio__list-text">Статичный сайт</p>
+            <img
+              className="portfolio__list-icon"
+              src={arrow}
+              alt="стрелка для перехода по ссылке"
+            />
+          </Link>
+        </li>
+        <li className="portfolio__list-item">
+          <Link className="portfolio__list-link" to="https://brododigitale.github.io/russian-travel/">
+            <p className="portfolio__list-text">Адаптивный сайт</p>
+            <img
+              className="portfolio__list-icon"
+              src={arrow}
+              alt="стрелка для перехода по ссылке"
+            />
+          </Link>
+        </li>
+        <li className="portfolio__list-item">
+          <Link className="portfolio__list-link" to="https://github.com/BrodoDigitale/react-mesto-api-full">
+            <p className="portfolio__list-text">Одностраничное приложение</p>
+            <img
+              className="portfolio__list-icon"
+              src={arrow}
+              alt="стрелка для перехода по ссылке"
+            />
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
 }
--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -3,7 +3,7 @@ import "./SectionHeader.css"
 
 export function SectionHeader(props) {
     return(
-        <div className="sectionHeader__wrapper">
+        <div className={`sectionHeader ${props.modificator}`}>
             <h2 className="sectionHeader__text">{props.title}</h2>
         </div>
     )
