1dfac831d5f1462fb76330033197b55dde2e1d40
Julia Sensi <sensi.julia@gmail.com>
2022-02-01 13:31:20 +0100

    feat: add PageNotFound component

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,6 +1,14 @@
-export function PageNotFound(props) {
+import "./PageNotFound.css"
+import { useHistory } from 'react-router-dom';
+
+export function PageNotFound() {
+    const history = useHistory();
+
     return(
-    <div>
-    </div>
+    <main className="pageNotFound">
+        <h1 className="pageNotFound__header">404</h1>
+        <p className="pageNotFound__text">Страница не найдена</p>
+        <button className="pageNotFound__button" to={history.goBack}>Назад</button>
+    </main>
     )
 }
