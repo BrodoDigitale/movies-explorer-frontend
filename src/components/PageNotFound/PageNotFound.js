38d2326d08f99a9fb009431b6c6e65bf64844d1c
Julia Sensi <sensi.julia@gmail.com>
2022-02-03 01:51:52 +0100

    style: add adaptive layout to PageNotFound and HamburgerMenu

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -6,8 +6,10 @@ export function PageNotFound() {
 
     return(
     <main className="pageNotFound">
+        <div className="pageNotFound__title-wrapper">
         <h1 className="pageNotFound__header">404</h1>
         <p className="pageNotFound__text">Страница не найдена</p>
+        </div>
         <button className="pageNotFound__button" to={history.goBack}>Назад</button>
     </main>
     )
